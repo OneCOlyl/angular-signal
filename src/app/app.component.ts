237461503666093aeb98abb5af4f23f889b39267
ssb <ssb@sibdigital.net>
2025-07-16 07:45:40 +0800

    feature/AS-3: add new function to select product/category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { SelectedItemComponent } from './components/selected-item/selected-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, SelectedItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
